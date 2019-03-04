d1918030594a415e73f91bf192c6fcbe1469a637
Benjamin <bscholtz.bds@gmail.com>
2019-03-04 22:14:01 +0200

    Remove .idea & shell.nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,24 @@
-/home/bscholtz/shells/rust.nix
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+with nixpkgs; stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [ 
+        #nixpkgs.latest.rustChannels.nightly.rust 
+        nixpkgs.latest.rustChannels.stable.rust
+        rustfmt
+        #rustup
+        #cargo 
+    ];
+    shellHook = ''
+        alias help="
+            echo 'cargo new _'
+            echo 'cargo build'
+            echo 'cargo check'
+            echo 'cargo run'
+            echo 'cargo build --release'
+        ";
+        cd $HOME/workspace/Rust;
+    '';
+}
